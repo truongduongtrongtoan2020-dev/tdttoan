feeaad68af3f86fc4c712c83138600c5e8907c1d
Twoan <truongduongtrongtoan2020@gmail.com>
2026-05-23 19:04:25 +0700

    lab02 transposition

--- lab_02/api.py
+++ lab_02/api.py
@@ -1,3 +1,4 @@
+from cipher.transposition import TranspositionCipher
 from cipher.playfair import PlayFairCipher
 from cipher.railfence import RailFenceCipher
 from cipher.vigenere import VigenereCipher
@@ -88,6 +89,25 @@ def playfair_decrypt():
     matrix = playfair_cipher.create_playfair_matrix(key)
     decrypted_text = playfair_cipher.playfair_decrypt(cipher_text, matrix)
     return jsonify({'decrypted_text': decrypted_text})  
+
+# TRANSPOSITION CIPHER ALGORITHM
+transposition_cipher = TranspositionCipher()    
+@app.route('/api/transposition/encrypt', methods=['POST'])
+def transposition_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = transposition_cipher.encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/transposition/decrypt', methods=['POST'])
+def transposition_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
+
 
 
 

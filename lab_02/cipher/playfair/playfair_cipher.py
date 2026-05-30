a632749b56f9eaf1b98b60f78e6ab138de22ca59
tdttoan <truongduongtrongtoan2020@gmail.com>
2026-05-30 13:44:26 +0700

    fix loi

--- lab_02/cipher/playfair/playfair_cipher.py
+++ lab_02/cipher/playfair/playfair_cipher.py
@@ -3,7 +3,7 @@ class PlayFairCipher:
         pass
         
     def create_playfair_matrix(self, key):
-        key = key.replace("J", "I") # Chuyển "J" thành "I" trong khóa
+        key = key.replace("J", "I")
         key = key.upper()
         key_set = set(key)
         alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
@@ -26,14 +26,13 @@ class PlayFairCipher:
                     return row, col
 
     def playfair_encrypt(self, plain_text, matrix):
-        # Chuyển "J" thành "I" trong văn bản đầu vào
         plain_text = plain_text.replace("J", "I")
         plain_text = plain_text.upper()
         encrypted_text = ""
         
         for i in range(0, len(plain_text), 2):
             pair = plain_text[i:i+2]
-            if len(pair) == 1: # Xử lý nếu số lượng ký tự lẻ
+            if len(pair) == 1:
                 pair += "X"
             row1, col1 = self.find_letter_coords(matrix, pair[0])
             row2, col2 = self.find_letter_coords(matrix, pair[1])
@@ -63,7 +62,6 @@ class PlayFairCipher:
                 decrypted_text += matrix[row1][col2] + matrix[row2][col1]
                 
         banro = ""
-        # Loại bỏ ký tự 'X' nếu nó là ký tự cuối cùng và là ký tự được thêm vào
         for i in range(0, len(decrypted_text)-2, 2):
             if decrypted_text[i] == decrypted_text[i+2]:
                 banro += decrypted_text[i]
